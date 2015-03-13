69fcaa03f1cf37cc6b99a9ab65e258b24bc15097
Watal M. Iwasaki <heavy.watal@gmail.com>
2015-03-13 23:53:47 +0900

    Implement class Individual

--- simulation.js
+++ simulation.js
@@ -18,12 +18,32 @@ var random;
 var simulation;
 (function (simulation) {
     var Individual = (function () {
-        function Individual(gamete1) {
-            this.gamete1 = gamete1;
+        function Individual(_zygote, _fitness) {
+            if (_zygote === void 0) { _zygote = ['ATGC', 'ATGC']; }
+            if (_fitness === void 0) { _fitness = 0; }
+            this._zygote = _zygote;
+            this._fitness = _fitness;
             console.log('constructor');
+            this._traits = [15, 15];
+            this._flight = 15 + this._traits[1] - this._traits[0];
+            this.print();
         }
         Individual.prototype.print = function () {
-            console.log(this.gamete1);
+            console.log(this._zygote);
+            console.log(this.phenotype());
+            console.log(this.fitness());
+        };
+        Individual.prototype.fitness = function () {
+            return this._fitness;
+        };
+        Individual.prototype.phenotype = function () {
+            return this._traits.concat(this._flight);
+        };
+        Individual.prototype.flight = function () {
+            this._fitness = this._flight / 31;
+        };
+        Individual.prototype.gametogenesis = function () {
+            return '';
         };
         return Individual;
     })();
@@ -33,7 +53,7 @@ var simulation;
             this.size = size;
             this.members = [];
             for (var i = 0; i < size; ++i) {
-                this.members.push(new Individual(String(i)));
+                this.members.push(new Individual());
             }
         }
         Population.prototype.print = function () {
--- simulation.ts
+++ simulation.ts
@@ -15,13 +15,35 @@ module random {
 
 module simulation {
 
-export class Individual {
-    fitness: number;
 
-    constructor(private gamete1: string) {
+export class Individual {
+    private _traits: number[];
+    private _flight: number;
+
+    constructor(
+        private _zygote: string[] = ['ATGC', 'ATGC'],
+        private _fitness: number = 0
+    ) {
         console.log('constructor');
+        this._traits = [15, 15];
+        this._flight = 15 + this._traits[1] - this._traits[0];
+        this.print();
     }
-    print() {console.log(this.gamete1);}
+    print() {
+        console.log(this._zygote);
+        console.log(this.phenotype());
+        console.log(this.fitness());
+    }
+    fitness(): number {return this._fitness;}
+    phenotype(): number[] {return this._traits.concat(this._flight);}
+
+    flight() {
+        this._fitness = this._flight / 31;
+    }
+
+    gametogenesis(): string {
+        return '';
+    }
 }
 
 export class Population {
@@ -30,7 +52,7 @@ export class Population {
     constructor(private size: number) {
         this.members = [];
         for (var i=0; i<size; ++i) {
-            this.members.push(new Individual(String(i)));
+            this.members.push(new Individual());
         }
     }
     print() {console.log(this.members);}
